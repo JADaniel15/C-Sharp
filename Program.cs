029b4e18c9c0ca366187029b8269632a0fcd563d
joao augusto <89113486+JADaniel15@users.noreply.github.com>
2022-05-02 20:36:09 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Estud\tonauta");
-Console.WriteLine("Estud\bonauta");
-Console.WriteLine("Estudonauta\a");
-Console.WriteLine("Estudonau\rta");
-Console.WriteLine("\\Estudonauta\\");
-Console.WriteLine(@"\Estudonauta\");
-Console.WriteLine("\"Estudonauta\"");
-Console.ReadKey();
+byte idade = 25;
+ushort publico = 45_239;
+float media = 4.5f;
+decimal estrela = 4.9847373723m;
+bool aprovado = false;
 
+int inteiro;
+int salárioBruto;
+
+Console.WriteLine();
+Console.ReadKey(); 
